41e5993c4b23e5d0377ab0a1caa45172c0936db4
DiegoM-Hack <santitodiego12@gmail.com>
2026-07-09 15:36:27 -0500

    login

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../screens/auth/login_screen.dart';
+import '../../screens/auth/register_screen.dart';
+//import '../../screens/auth/splash_screen.dart';
+import '../../screens/home/home_screen.dart';
+//import '../../screens/profile/profile_screen.dart';
+
+class AppRoutes {
+  // Nombres de las rutas
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+  static const String profile = '/profile';
+
+  /// Rutas de la aplicación
+  static Map<String, WidgetBuilder> routes = {
+    //splash: (context) => const SplashScreen(),
+    login: (context) => const LoginScreen(),
+    register: (context) => const RegisterScreen(),
+    home: (context) => const HomeScreen(),
+    //profile: (context) => const ProfileScreen(),
+  };
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text("SafeWalk"),
+      ),
+
+      body: const Center(
+        child: Text(
+          "Bienvenido",
+        ),
+      ),
+
+    );
+
+  }
+
+}
